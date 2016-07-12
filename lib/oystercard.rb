216c638145830d015537de1af7720dfee495e33d
Laurence kite <laurencekite@Laurences-MacBook-Pro-2.local>
2016-07-12 15:06:01 +0100

    updated touch in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-
+  MAX_LIMIT = 90
   attr_reader :balance
 
   def initialize
@@ -7,7 +7,15 @@ class Oystercard
   end
 
   def top_up(amount)
+  fail "Maximum balance of #{MAX_LIMIT} exceeded" if @balance + amount > MAX_LIMIT
     @balance += amount
   end
 
+  def deduct(amount)
+    @balance -= amount
+  end
+
+ def touch_in
+   true
+ end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,21 +1,33 @@
 require 'oystercard'
 
 describe Oystercard do
+
   let(:oyster) {described_class.new}
   it 'it has a balance of zero' do
-  expect(subject.balance).to  eq(0)
+  expect(subject.balance).to eq(0)
   end
-  # it 'responds to top_up' do
-  #   expect(oyster).to  respond_to(:top_up).with(1).argument
-  # end
-#re written the above using this:
+
   describe '#top_up' do
     it {is_expected.to respond_to(:top_up).with(1).argument}
     it 'can top up the balance' do
-      expect{subject.top_up 1}.to change {subject.balance}.by 1
+      expect{subject.top_up(1)}.to change {subject.balance}.by 1
     end
 
-  
+     it 'stops topping up over the limit of 91'  do
+       maxlimit = Oystercard::MAX_LIMIT
+       expect{subject.top_up(maxlimit + 1)}.to raise_error "Maximum balance of #{maxlimit} exceeded"
+     end
+   end
 
-  end
+    describe 'deduct' do
+      it 'will reduce balance by 2' do
+        expect{subject.deduct(2)}.to change {subject.balance}.by -2
+      end
+    end
+    describe 'touch_in' do
+       it 'allows a user to touch_in' do
+         # set instance variable here
+         expect(subject.touch_in).to eq(true)
+      end
+    end
 end
